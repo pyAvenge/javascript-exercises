41d4df6b88f327904547dd195f24e67d93f12c98
sylar300 <amer.fetic.s6@gmail.com>
2022-05-24 10:30:14 +0200

    exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(a) {
 
+    if (a%4==0){
+        if(a%100 !=0){
+            return true
+        }
+        else if (a%400 == 0){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else if (a%100 == 0 && a%400 == 0){
+        return true
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
