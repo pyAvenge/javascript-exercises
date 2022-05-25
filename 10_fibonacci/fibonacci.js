a17626eef4021002cbf8e57f31183fb3dc73807f
sylar300 <amer.fetic.s6@gmail.com>
2022-05-25 10:14:38 +0200

    Exercise 9 finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let b = parseInt(a)
+    if (b<0){
+        return "OOPS"
+    }
+    else if ((b == 0) || (b == 1)){
+        return 1
+    }
+    else{
+        fibZero = 0
+        fibOne = 1
+            for (i=0;i<b-1;i++){
+                numberFib = fibZero+fibOne
+                fibZero = fibOne
+                fibOne = numberFib
+            }
+        }
+return numberFib
 };
 
 // Do not edit below this line
